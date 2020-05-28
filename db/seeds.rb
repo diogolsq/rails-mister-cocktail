a2efcc90c5cf6f28205eb2db20f3ce278c515205
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-05-28 12:53:20 -0300

    creating the seed by pulling the ingredient data from a API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+listingredient_serialized = open(url).read
+listingredient = JSON.parse(listingredient_serialized)
+
+# puts "#{ingredient['name']} - #{ingredient['bio']}"
+
+puts 'pulling ingredients from the api'
+
+listingredient['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+# Ingredient.create(name: 'Lemon')
+# Ingredient.create(name: 'Ice')
+# Ingredient.create(name: 'Mint Leaves')
+# Ingredient.create(name: 'Gin')
+# Ingredient.create(name: 'Vodka')
+# Ingredient.create(name: 'Soda Water')
+# Ingredient.create(name: 'Sugar Syrup')
+# Ingredient.create(name: 'Cider')
+# Ingredient.create(name: 'Apple')
+# Ingredient.create(name: 'Orange')
+# Ingredient.create(name: 'Camomile Tea Bag')
+# Ingredient.create(name: 'Sweet Vermouth')
+# Ingredient.create(name: 'Campari')
+# Ingredient.create(name: 'Blue Curacau')
+# Ingredient.create(name: 'Lime')
+# Ingredient.create(name: 'Angostura Bitters')
+# Ingredient.create(name: 'Tequila')
+# Ingredient.create(name: 'Rum')
+# Ingredient.create(name: 'Cointreau')
+# Ingredient.create(name: 'Ginger Ale')
+
+puts 'done'
