51e16c0e60c5e511af83b8e2ad77bca31f39edb1
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-05-28 13:12:45 -0300

    creating the index, view with the navbar, very basic for now

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,35 @@
+class CocktailsController < ApplicationController
+
+  before_action :find_cocktail, only: [:show, :edit, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktails_params)
+    @cocktail.save
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktails_params
+    params.require(:cocktail).permit(:name, :address, :category)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+
+  resources :cocktails, only: [:index, :new, :show, :create] do
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
