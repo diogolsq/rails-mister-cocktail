a7b02837153c442e6366d9eb6b57576e563ccbb4
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-05-29 16:24:11 -0300

    fixing the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,10 +44,10 @@ end
 
 puts 'done'
 
-puts 'creating the default drinks'
+# puts 'creating the default drinks'
 
-coktails[['Mojito', 'Place mint leaves and 1 lime wedge into a sturdy glass. Use a muddler to crush the mint and lime to release the mint oils and lime juice. Add 2 more lime wedges and the sugar, and muddle again to release the lime juice. Do not strain the mixture. Fill the glass almost to the top with ice. Pour the rum over the ice, and fill the glass with carbonated water. Stir, taste, and add more sugar if desired. Garnish with the remaining lime wedge.', 'tea-mojito.png'],
-         ['Martini', 'Stir the gin or vodka, dry vermouth and a little ice together or put them in a cocktail shaker to combine. Strain into a chilled martini glass. Serve with an olive on a cocktail stick or a twist of lemon peel.', 'martini.png'],
-         ['Negroni', 'Add all the ingredients into a mixing glass with ice, and stir until well-chilled. Strain into a rocks glass filled with large ice cubes. Garnish with an orange peel.', 'negroni.png' ],
-         ['Caipirinha', 'Put the lime wedges in a large jug with the sugar, then use a muddler or the end of a rolling pin to break them down. Release as much juice from the wedges as possible, and let it mix with the sugar to create a syrup – you can also do this with a large pestle and mortar. Discard the lime peel. Divide the syrup and cachaça between four glasses, then top with the crushed ice. Serve garnished with lime wedges, if you like.', 'caipirinha.png'],
-        ]
+# cocktails = [['Mojito', 'Place mint leaves and 1 lime wedge into a sturdy glass. Use a muddler to crush the mint and lime to release the mint oils and lime juice. Add 2 more lime wedges and the sugar, and muddle again to release the lime juice. Do not strain the mixture. Fill the glass almost to the top with ice. Pour the rum over the ice, and fill the glass with carbonated water. Stir, taste, and add more sugar if desired. Garnish with the remaining lime wedge.', 'tea-mojito.png'],
+#          ['Martini', 'Stir the gin or vodka, dry vermouth and a little ice together or put them in a cocktail shaker to combine. Strain into a chilled martini glass. Serve with an olive on a cocktail stick or a twist of lemon peel.', 'martini.png'],
+#          ['Negroni', 'Add all the ingredients into a mixing glass with ice, and stir until well-chilled. Strain into a rocks glass filled with large ice cubes. Garnish with an orange peel.', 'negroni.png' ],
+#          ['Caipirinha', 'Put the lime wedges in a large jug with the sugar, then use a muddler or the end of a rolling pin to break them down. Release as much juice from the wedges as possible, and let it mix with the sugar to create a syrup – you can also do this with a large pestle and mortar. Discard the lime peel. Divide the syrup and cachaça between four glasses, then top with the crushed ice. Serve garnished with lime wedges, if you like.', 'caipirinha.png'],
+#         ]
