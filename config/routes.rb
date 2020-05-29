11b74304eb1358cc5674225c884a6bd3b32f0bff
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-05-29 12:41:22 -0300

    fixing landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-
   resources :cocktails, only: [:index, :new, :show, :create, :edit, :update] do
     resources :doses, only: [:new, :create, :destroy]
   end
+  root to: 'cocktails#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
