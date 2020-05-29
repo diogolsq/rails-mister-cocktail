088c182ee21254f7ff7aa1e7503ebc55b0e3d2ab
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-05-29 07:35:20 -0300

    going back to previous version

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,6 +22,7 @@ document.addEventListener('turbolinks:load', () => {
   initUpdateNavbarOnScroll();
   loadDynamicBannerText();
   initSelect2();
+  AOS.init();
 });
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
